6fc3ab9f996956f98054dfd4118126ab4814897a
Prelang Builder <builder@prelang.com>
2016-04-08 15:06:56 +0000

    Generated model Ingredient with migration

---
+++ app/models/ingredient.rb
@@ -0,0 +1,4 @@
+class Ingredient < ActiveRecord::Base
+  belongs_to :stock_type
+  belongs_to :stock_type
+end
---
+++ spec/factories/ingredients.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :ingredient do
+    quantity "9.99"
+    unit "MyString"
+    stock_type nil
+    stock_type nil
+  end
+end
